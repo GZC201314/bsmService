c4ce08fc9455f754de58d9d4a7fd30c0fa6f3fb7
GZC <gzc201314@163.com>
2022-06-27 11:28:13 +0800

    数据库定时备份

--- src/main/java/org/bsm/task/job/DBbackupJob.java
+++ src/main/java/org/bsm/task/job/DBbackupJob.java
@@ -1,9 +1,9 @@
 package org.bsm.task.job;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.codec.Base64;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.RandomStringUtils;
+import org.bsm.pagemodel.PageGiteeApiCaller;
+import org.bsm.service.IGiteeService;
 import org.bsm.utils.RedisUtil;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobDataMap;
@@ -18,15 +18,17 @@ import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-import javax.activation.DataHandler;
-import javax.activation.FileDataSource;
 import javax.annotation.Resource;
-import javax.mail.Multipart;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -47,6 +49,9 @@ public class DBbackupJob extends QuartzJobBean {
     @Autowired
     private TemplateEngine templateEngine;
 
+    @Resource
+    private IGiteeService giteeService;
+
     @Autowired
     RedisUtil redisUtil;
 
@@ -55,6 +60,7 @@ public class DBbackupJob extends QuartzJobBean {
         InputStream inputStream = null;
         InputStreamReader inputStreamReader = null;
         BufferedReader bf = null;
+        String fileUrl = "";
         try {
             String filePath = (String) redisUtil.hget("bsm_config", "DB_BACKUP_DIR");
             String emailaddress = (String) redisUtil.hget("bsm_config", "DBA_EMAIL");
@@ -62,44 +68,57 @@ public class DBbackupJob extends QuartzJobBean {
             JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
             log.info(context.getScheduler().getSchedulerInstanceId());
             log.info("taskname= {}", context.getJobDetail().getKey().getName());
-            log.info("执行时间= {}", new Date());
+            log.info("执行时间= {}", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
 
             String containerName = (String) jobDataMap.get("containerName");
             String dbName = (String) jobDataMap.get("dbname");
             String username = (String) jobDataMap.get("username");//用户名
             String password = (String) jobDataMap.get("password");//密码
-            File uploadDir = new File(filePath);
-            if (!uploadDir.exists()) {
-                uploadDir.mkdirs();
-            }
-
             String fileName = dbName + new java.util.Date().getTime() + ".sql";
             // linux执行定时任务的时候没有终端设备，TTY一词源于Teletypes,或teletypewriters。
             // 其实出现该错误和我们的一个习惯有关，一般来说我们启动容器后要与容器进行交互操作，
             // 这是，就要加上"-it"这个参数，而在定时任务中，如果让脚本在后台运行，就没有可交互的终端，
             // 这就会引发如题所示错误，解决办法就是去掉“-it”这个参数。
-            String cmd = "docker exec -i " + containerName + " mysqldump -u" + username + " -p" + password + " " + dbName;
+            String cmd = "docker exec -i " + containerName + " mysqldump -u" + username + " -p" + password + " " + dbName+" -r "+filePath+fileName;
             log.info(cmd);
             try {
+                long now1 = new Date().getTime();
                 Process process = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", cmd});
+
                 int result = process.waitFor();
-                inputStream = process.getInputStream();
-                inputStreamReader = new InputStreamReader(inputStream);
-                bf = new BufferedReader(inputStreamReader);
-                StringBuilder sb = new StringBuilder();
+                long now = new Date().getTime();
+                log.info("本次数据库备份花费时间是，{}毫秒",now-now1);
 
-                bf.lines().forEach(str ->{
-                    sb.append(str).append("\n");
-                });
-
-                log.error(sb.toString());
-
-                String path = filePath + dbName + "_" + new Date().getTime() + ".sql";
-                //把备份的数据插入到文件中
-                File file = FileUtil.writeBytes(sb.toString().getBytes(), path);
-
-                if (result == 0 && file.exists()) {
+                if (result == 0) {
                     log.info("数据库备份成功！");
+
+                    // 把备份文件从容器中移动出来
+                    String cpCmd = "docker cp "+containerName+":"+filePath+fileName+" "+filePath;
+                    Process cpProcess = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", cpCmd});
+                    int cpResult = cpProcess.waitFor();
+                    File file =  new File(filePath+fileName);
+                    if(cpResult == 0 && file.exists()){
+                        PageGiteeApiCaller pageGiteeApiCaller = new PageGiteeApiCaller();
+                        pageGiteeApiCaller.setOwner("GZC201314");
+                        pageGiteeApiCaller.setPath("BSM/DBBackUp/"+fileName);
+                        pageGiteeApiCaller.setRepo("tuchuang");
+                        String fileBase64 = Base64.encode(Files.readAllBytes(file.toPath()));
+                        pageGiteeApiCaller.setContent(fileBase64);
+                        pageGiteeApiCaller.setMessage("数据库备份文件上传 " + LocalDateTime.now());
+                        fileUrl = giteeService.addFile(pageGiteeApiCaller);
+                        //删除备份文件,容器中的备份文件，服务器上的备份文件
+                        // 1.删除容器中的备份文件
+                        String rmCmd = "docker exec -i " + containerName + " rm "+filePath+fileName;
+                        Process rmProcess = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", rmCmd});
+                        boolean delResult = file.delete();
+                        int rmResult = rmProcess.waitFor();
+                        if(!(rmResult ==0 && delResult)){
+                            return;
+                        }
+
+
+                    }
+
                     // 给 DBA 发送备份成功的邮件
                     MimeMessage message;
                     try {
@@ -111,6 +130,7 @@ public class DBbackupJob extends QuartzJobBean {
                         // 处理邮件模板
                         Context emailContext = new Context();
                         emailContext.setVariable("datetime",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+                        emailContext.setVariable("fileurl",fileUrl);
                         String template = templateEngine.process("mail/DBbackupTemplate", emailContext);
 
                         // 6，设置邮件内容，混合模式
@@ -129,17 +149,6 @@ public class DBbackupJob extends QuartzJobBean {
                         MimeBodyPart htmlPart = new MimeBodyPart();
                         bodyMultipart.addBodyPart(htmlPart);
                         htmlPart.setContent(template, "text/html;charset=UTF-8");
-
-                        //设置备份文件
-                        MimeBodyPart filePart = new MimeBodyPart();
-                        FileDataSource dataSource = new FileDataSource(file);
-                        DataHandler dataHandler = new DataHandler(dataSource);
-                        // 文件处理
-                        filePart.setDataHandler(dataHandler);
-                        // 附件名称
-                        filePart.setFileName(file.getName());
-                        // 放入正文（有先后顺序，所以在正文后面）
-                        msgMultipart.addBodyPart(filePart);
 
 
                         javaMailSender.send(message);
